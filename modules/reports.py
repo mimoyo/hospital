04f657e1ed9179350f5928cb5603a17b9eb2b0db
Andrey Baev <andrey@frostone.ru>
2025-07-05 14:05:11 +0200

    Some fixes

--- modules/reports.py
+++ modules/reports.py
@@ -7,21 +7,25 @@ reports_bp = Blueprint('reports', __name__)
 @reports_bp.route('/reports/patient_prescriptions')
 def report_patient_prescriptions():
     results = db.session.query(
-        Patient.full_name,
+        Patient.last_name,
+        Patient.first_name,
+        Patient.middle_name,
         func.count(Prescription.id),
         func.coalesce(func.sum(Prescription.total_price), 0)
     ).join(MedicalRecord, MedicalRecord.patient_id == Patient.id) \
      .join(Prescription, Prescription.history_id == MedicalRecord.id) \
-     .group_by(Patient.full_name).all()
+     .group_by(Patient.last_name, Patient.first_name, Patient.middle_name).all()
 
     return render_template('reports/patient_prescriptions/report_patient_prescriptions.html', data=results)
 
 @reports_bp.route('/reports/doctor_appointments')
 def report_doctor_appointments():
     results = db.session.query(
-        Doctor.full_name,
+        Doctor.last_name,
+        Doctor.first_name,
+        Doctor.middle_name,
         func.count(Appointment.id)
-    ).join(Appointment).group_by(Doctor.full_name).all()
+    ).join(Appointment).group_by(Doctor.last_name, Doctor.first_name, Doctor.middle_name).all()
 
     return render_template('reports/doctor_appointments/report_doctor_appointments.html', data=results)
 
